71a2ebb68f52b3bf3945f7d95921a04fc077af39
muksis <60219890+muksis@users.noreply.github.com>
2022-04-21 13:59:07 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+      info: {
+          title: 'Library API',
+          version: '1.0.0'
+      }
+  },
+  apis: ['app.js']
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     description: Get all books
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+app.get('/books', (req, res) => {
+    res.send([
+        {
+            isbn: '9781781100486',
+            title: 'Harry Potter and the Sorcerer\'s Stone',
+            author: 'J.K. Rowling',
+            publisher: 'Scholastic'
+        }
+    ]);
+});
+
+app.listen(3000, () => {
+    console.log('Running on port 3000');
+});
